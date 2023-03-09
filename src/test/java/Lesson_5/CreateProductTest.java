bb21336de93092f0ffcd0c0ae9a422bb8f1cf8b2
maksimuspb <101411090+maksimuspb@users.noreply.github.com>
2023-03-09 16:11:55 +0300

    Сделал дз 6

--- src/test/java/Lesson_5/CreateProductTest.java
+++ src/test/java/Lesson_5/CreateProductTest.java
@@ -1,84 +1,106 @@
-package Lesson_5;
-
-import Lesson_5.Api.ProductService;
-import Lesson_5.DTO.Product;
-import Lesson_5.Util.RetrofitUtils;
-import jdk.jfr.Category;
-import lombok.SneakyThrows;
-import okhttp3.ResponseBody;
-import org.hamcrest.CoreMatchers;
-import org.junit.jupiter.api.*;
-import retrofit2.Response;
 
 import java.io.IOException;
 
-import static org.hamcrest.MatcherAssert.assertThat;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import retrofit.api.MiniMarketApi;
+import retrofit.dto.Category;
+import retrofit.dto.ProductDto;
+import retrofit.utils.RetrofitGetter;
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import java.util.List;
+import io.restassured.internal.common.assertion.Assertion;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.Assert.assertEquals;
 
-public class CreateProductTest {
-    static ProductService productService;
-    Product product = null;
-    Faker faker = new Faker();
-    int id;
+import org.opentest4j.AssertionFailedError;
 
-    @BeforeAll
-    static void beforeAll() {
-        productService = RetrofitUtils.getRetrofit()
-                .create(ProductService.class);
-    }
 
-    @BeforeEach
-    void setUp() {
-        product = new Product()
-                .withTitle(faker.food().ingredient())
-                .withCategoryTitle("Food")
-                .withPrice((int) (Math.random() * 10000));
-    }
+public class ApiTest {
 
-    @Test
-    void createProductInFoodCategoryTest() throws IOException {
-        Response<Product> response = productService.createProduct(product)
-                .execute();
-        id = response.body().getId();
-        assertThat(response.isSuccessful(), CoreMatchers.is(true));
-    }
+    private final MiniMarketApi api;
 
-    @SneakyThrows
-    @AfterEach
-    void tearDown() {
-        Response<ResponseBody> response = productService.deleteProduct(id).execute();
-        assertThat(response.isSuccessful(), CoreMatchers.is(true));
+    public ApiTest() throws IOException {
+        Retrofit retrofit = new RetrofitGetter().getInstance();
+        api = retrofit.create(MiniMarketApi.class);
     }
 
     @Test
-    void testGetProduct() throws IOException {
+    @DisplayName("Create product")
+    void testCreateProduct() throws IOException {
 
-        product = new Product()
-                .id(7)
-                .title("Meat balls")
-                .categoryTitle(Category.food().getTitle())
-                .price(15L)
+        ProductDto dto = ProductDto.builder()
+                .title("Candies")
+                .categoryTitle(Category.FOOD.getTitle())
+                .price(50L)
                 .build();
 
-
-        Response<Product> response = api.getProduct(dto.getId()).execute();
+        Response<ProductDto> response = api.createProduct(dto).execute();
         Long id = response.body().getId();
 
+        ProductDto actually = api.getProduct(id).execute().body();
 
+        assertEquals(dto.getTitle(), actually.getTitle());
+        assertEquals(dto.getPrice(), actually.getPrice());
+        assertEquals(dto.getCategoryTitle(), actually.getCategoryTitle());
 
-
-
+        api.deleteProduct(id).execute();
     }
+
     @Test
+    @DisplayName("Getting product info")
+    void testGetProduct() throws IOException {
 
-    void testDeleteProduct() throws IOException {
-
-        product  = new Product()
+        ProductDto dto = ProductDto.builder()
+                .id(7)
                 .title("Meat balls")
-                .categoryTitle(Category.food.getTitle())
+                .categoryTitle(Category.FOOD.getTitle())
                 .price(15L)
                 .build();
 
-        Response<Product> response = api.createProduct(dto).execute();
+        Response<ProductDto> response = api.getProduct(dto.getId()).execute();
+        Long id = response.body().getId();
+
+        ProductDto actually = api.getProduct(id).execute().body();
+
+        assertEquals(dto.getTitle(), actually.getTitle());
+        assertEquals(dto.getPrice(), actually.getPrice());
+        assertEquals(dto.getCategoryTitle(), actually.getCategoryTitle());
+
+    }
+
+    @Test
+    @DisplayName("Updating product info")
+    void testUpdateProduct() throws IOException {
+
+        ProductDto dto = ProductDto.builder()
+                .id(8)
+                .title("Eggs")
+                .categoryTitle(Category.FOOD.getTitle())
+                .price(3L)
+                .build();
+
+        Response<ProductDto> response = api.updateProduct(dto).execute();
+        Long id = response.body().getId();
+
+        ProductDto actually = api.getProduct(id).execute().body();
+
+        assertEquals(dto.getPrice(), actually.getPrice());
+    }
+
+    @Test
+    @DisplayName("Delete product")
+    void testDeleteProduct() throws IOException {
+
+        ProductDto dto = ProductDto.builder()
+                .title("Meat balls")
+                .categoryTitle(Category.FOOD.getTitle())
+                .price(15L)
+                .build();
+
+        Response<ProductDto> response = api.createProduct(dto).execute();
 
         Long id = response.body().getId();
 
@@ -86,7 +108,7 @@ public class CreateProductTest {
 
         api.getProduct(id).execute();
 
-
+        new AssertionError("There is no product with id: " + dto.getId());
     }
 }
 
